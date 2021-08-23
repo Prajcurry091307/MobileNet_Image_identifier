4d6b21ba961f12f00aca248fed39fcab39e14a6e
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-08-23 16:25:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,35 @@ Webcam.set({
 
 var camera= document.getElementById("camera");
 Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>'; 
+    });
+
+}
+
+console.log('ml5 version:', ml5.version)
+
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
+
+function check()
+{
+    var img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
